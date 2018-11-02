59d993cfd80719b108f91cd5a9fee2cf92aba835
zhanghanwen1 <1987463587a>
2018-11-02 11:35:18 +0800

    Spring Tx的使用，包括XML和注解两种方式

---
+++ spring8x1/src/main/java/cap/dao/AdminDAO.java
@@ -0,0 +1,13 @@
+package cap.dao;
+
+import cap.bean.Admin;
+
+import java.util.List;
+
+public interface AdminDAO {
+    List<Admin> findAdmins();
+    Admin findById(int id);
+    int updateAdmin(Admin admin);
+    int delAdmin(int id);
+    int addAdmin(Admin admin);
+}
